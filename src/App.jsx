2a5ac916f73c2507da7ee55fa1da4d7ee1532d79
Zaky Hossain <zaky15-3610@diu.edu.bd>
2026-09-19 08:42:26 +0600

    added models json data & models component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
+import Models from "./components/Models";
+
+
+const getModels =
+
+
+
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Banner />
-
+      <Models />
       <Footer />
     </>
   );
